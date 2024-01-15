4cab7b78df6dae7df5fa38ebf7941fdb5730aac9
Frogalove <143967544+Frogalove@users.noreply.github.com>
2024-01-15 22:29:01 +0100

    verandering components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 import './App.css'
+import {useEffect, useState} from "react";
+import axios from'axios';
+import PokemonCard from "./assets/components/pokemon/PokemonCard.jsx";
+
 
 function App() {
+  const [pokemonList, setPokemonList] = useState([]);
 
+  useEffect (() => {
+    async function fetchPokemonCard() {
+      try {
+
+        const result = await axios.get('https://pokeapi.co/api/v2/pokemon/')
+        setPokemonList(result.data);
+        console.log(result);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
+    fetchPokemonCard();
+  }, []);
   return (
     <>
-      <h1>Gotta catch em all!</h1>
+      <h1>Pokemon List</h1>
+      <ul>
+        {pokemonList.map((pokemon,index) => (
+          <li key={index}>{pokemon.name}</li>
+      ))}
+      </ul>
+
+
+    <PokemonCard/>
     </>
-  )
+
+  );
 }
 
-export default App
+export default App;
---
+++ src/assets/components/pokemon/PokemonCard.jsx
@@ -0,0 +1,40 @@
+
+import {useEffect} from "react";
+import './PokemonCard.css';
+
+
+// Ditto
+// [afbeelding]
+// Moves: 146
+// Weight: 55
+// Abilities:
+// cute-charm
+// competitive
+// friend-guard
+  function PokemonCard() {
+  const [pokemon,setPokemon] = useState([]);
+
+  useEffect( () => {
+    async function fetchPokemonCard() {
+    try {
+    const result = await axios.get( 'https://pokeapi.co/api/v2/pokemon/ditto/')
+      setPokemon(result);
+      console.log(result)
+    } catch (e) {
+      console.error (e);
+
+    }
+       }
+       fetchPokemonCard();
+  }, []);
+
+ return (
+     <>
+      <h2>Poki {pokemon.name}</h2>
+
+     </>
+
+   );
+}
+
+export default PokemonCard;
